8844086e9dadc27ef77d182af2a2deac1fb8335d
isabelamn07 <isabelamn07@gmail.com>
2018-10-22 16:15:35 -0300

    update break pressure

--- fuzzy_confusao.py
+++ fuzzy_confusao.py
@@ -14,21 +14,28 @@ step3: create rules set
 Problem: Car 0 - 100 km/h
          distance: 0 - 100m
 
-         choose between break and presure ???
+         define the break pressure
 '''
 
 
 break_pressure = np.arange(0, 101)
-velocity = np.arange(0, 101)
+speed = np.arange(0, 101)
 x_distance = np.arange(0, 101)
 
+#define better mesures
 x_short_distance = fuzz.trimf(x_distance,[0, 10, 20])
 x_average_distace = fuzz.trimf(x_distance, [15, 45, 60])
 x_long_distance = fuzz.trimf(x_distance,[50, 75, 100])
 
-v_fast = fuzz.trimf(velocity, [80, 90, 100])
-v_avg = fuzz.trimf(velocity, [40, 60, 80])
-v_slow = fuzz.trimf(velocity, [1, 20, 40])
+#define better mesures
+v_fast = fuzz.trimf(speed, [80, 90, 100])
+v_avg = fuzz.trimf(speed, [40, 60, 80])
+v_slow = fuzz.trimf(speed, [1, 20, 40])
+
+#define better mesures
+light_break_pressure = fuzz.trimf(break_pressure, [0, 15, 30])
+medium_break_pessure = fuzz.trimf(break_pressure, [20, 40, 55])
+heavy_break_pressure = fuzz.trimf(break_pressure, [50, 75, 100])
 
 plt.figure()
 
@@ -43,7 +50,6 @@ plt.show()
 # Rules membership
 #rule1 = np.fmax(x_long_distance, v_fast)
 #activate_pressure
-
 
 
 
